c6ed0dae5055a2310b7a98f2aedf1bea72fe4903
Dustin Korzaan <dustin@korzaan.com>
2026-07-09 14:33:42 +0000

    Weather forecast all from core

--- api-dotnet/WeatherAPI/Program.cs
+++ api-dotnet/WeatherAPI/Program.cs
@@ -10,32 +10,8 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 // hack, because the default route is not working in codespaces, so redirect to the weatherforecast endpoint
 app.MapGet("/", () => Results.Redirect("/weatherforecast"));
 app.MapControllers();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
